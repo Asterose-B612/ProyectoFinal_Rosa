dbf1bf2a4c1da903df77ee81d1aa909cc09b9b40
KarinaR <decodikiaro@gmail.com>
2023-10-25 11:59:54 -0300

    modificacion en Cart y Chechout para solucionar problemas de la visualizacion del total final de la compra

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,11 @@
 import { useCartContext } from "../../context/CartContext";
 import CartItem from '../CartItem/CartItem'
-import { createContext } from "react";
 import {Link} from 'react-router-dom'
-import {useContext,useState} from "react";
-import { CartContext } from '../../context/CartContext'
 import './Cart.css';
 
 const Cart = () => {
   //tods las func y datos vienen del Cart Context
-    const {cart, clearCart, totalQuantity, total} = useCartContext(createContext)
+    const {cart, clearCart, totalQuantity, total} = useCartContext()
 //validacion de productos agregados
     if (totalQuantity === 0) {
         return(
@@ -23,7 +20,7 @@ const Cart = () => {
     return (
         <div className='cartContainer'>
         <div className='cartItemProd'>   { cart.map(p => <CartItem key={p.id}  {...p}/> ) } </div> 
-            <h3>Total: ${total}</h3>
+            <h3>Total: ${total()}</h3>
             <button onClick={ () => clearCart() } className='ButtonVaciar'> Vaciar Carrito </button>
             <Link to = '/checkout' className='ButtonCheckout'>Checkout</Link>
         </div>
--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -48,63 +48,63 @@ const Checkout = () => {
       return;
     }
 
-    try {
+    const createOrder = async ({ name, phone, email }) => {
+      setLoading(true);
+      
+      try {
       const objOrder = {
-        buyer: {
-          name,
-          phone,
-          email,
-        },
-        item: cart,
-        total: total(),
-        date: Timestamp.fromDate(new Date()),
+      buyer: {
+      name, phone, email
+      },
+      item: cart,
+      total: total,
+      date: Timestamp.fromDate(new Date()),
       };
-
+      
       const batch = writeBatch(db);
-
       const outOfStock = [];
-
-      const ids = cart.map((prod) => prod.id);
-
-      const productsRef = collection(db, 'products');
-
-      const productsAddedFromFirestore = await getDocs(
-        query(productsRef, where(documentId(), 'in', ids))
-      );
-
-      const { docs } = productsAddedFromFirestore;
-
-      docs.forEach((doc) => {
-        const dataDoc = doc.data();
-        const stockDb = dataDoc.stock;
-        const productAddedToCart = cart.find((prod) => prod.id === doc.id);
-        const prodQuantity = productAddedToCart?.quantity;
-
-        if (stockDb >= prodQuantity) {
-          batch.update(doc.ref, { stock: stockDb - prodQuantity });
-        } else {
-          outOfStock.push({ id: doc.id, ...dataDoc });
-        }
-      });
-
-      if (outOfStock.length === 0) {
-        await batch.commit();
-
-        const orderRef = collection(db, 'orders');
-
-        const orderAdded = await addDoc(orderRef, objOrder);
-
-        setOrderId(orderAdded.id);
-        clearCart();
+      const ids = cart.map (prod => prod.id)
+      const productsRef = collection (db, 'items')
+      const productsAdaptedFromFirestore = await getDocs (query(productsRef, where (documentId(), 'in, id')))
+      const { docs } = productsAdaptedFromFirestore
+      
+      docs.forEach (doc => {
+      const dataDoc = doc.data ()
+      const stockDb = dataDoc.stock
+      
+      const productsAdaptedToCart = cart.find (prod => prod.id === doc.id)
+      const prodQuantity = productsAdaptedToCart?.quantity
+      
+      if (stockDb >= prodQuantity) {
+      batch.update (doc.ref, {stock: stockDb - prodQuantity})
+      
       } else {
-        console.error('Hay productos fuera de stock');
+      outOfStock.push ({id: doc.id, ...dataDoc})
       }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+      
+      
+      })
+      
+      if (outOfStock.length === 0) {
+        await batch.commit()
+        
+        const orderRef = collection (db, 'orders')
+        const orderAdded = await addDoc (orderRef, objOrder)
+        
+        setOrderId (orderAdded.id)
+        clearCart ()
+        } else {
+        console.error ('No se encuentran en stock')
+        }
+        
+        } catch (error) {
+        console.error (error)
+        } finally {
+        setLoading (false)
+        }
+        
+        
+        };
 
   if (loading) {
     return <h1>Se está generando su orden...</h1>;
